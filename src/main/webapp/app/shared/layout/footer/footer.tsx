c90c8db99907733383e2a29dc826ee3ac4d257ad
Dragos Serban <49645354+moleguaco99@users.noreply.github.com>
2019-10-29 11:55:23 +0200

    First modifications for welcome page
    - modified ribbon and JHipster Logo

--- src/main/webapp/app/shared/layout/footer/footer.tsx
+++ src/main/webapp/app/shared/layout/footer/footer.tsx
@@ -8,7 +8,7 @@ const Footer = props => (
   <div className="footer page-content">
     <Row>
       <Col md="12">
-        <p>Your footer</p>
+        <p>UBBForum 2019</p>
       </Col>
     </Row>
   </div>
