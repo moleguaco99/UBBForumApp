c78d651eeaa69fe1a638b3f8c089c7e84ce26f7e
tamAdrian <t.adrian1998@gmail.com>
2019-12-06 13:45:19 +0200

    Added method to find all tagQuestions by question

--- src/main/java/src/repository/AnswerRepository.java
+++ src/main/java/src/repository/AnswerRepository.java
@@ -2,6 +2,8 @@ package src.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import src.domain.Answer;
+import java.util.List;
 
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
+    List<Answer> findAllByIdQuestionOrAnswer();
 }
--- src/main/java/src/repository/TagQuestionRepository.java
+++ src/main/java/src/repository/TagQuestionRepository.java
@@ -1,7 +1,10 @@
 package src.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import src.domain.Question;
 import src.domain.TagQuestion;
+import java.util.List;
 
 public interface TagQuestionRepository extends JpaRepository<TagQuestion, Long> {
+    List<TagQuestion> findAllByQuestion(Question question);
 }
