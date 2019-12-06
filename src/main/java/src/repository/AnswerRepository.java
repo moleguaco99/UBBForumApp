1bcaf280011fa370f73b69107914fa4b4a2137ac
tamAdrian <t.adrian1998@gmail.com>
2019-12-06 13:49:24 +0200

    Reformat code

--- src/main/java/src/repository/AnswerRepository.java
+++ src/main/java/src/repository/AnswerRepository.java
@@ -2,8 +2,6 @@ package src.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import src.domain.Answer;
-import java.util.List;
 
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
-    List<Answer> findAllByIdQuestionOrAnswer();
 }
