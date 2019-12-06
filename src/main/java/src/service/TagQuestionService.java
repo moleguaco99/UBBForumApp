b6fe9b20240a09f23fbafbb0820f123dfd01cadd
tamAdrian <t.adrian1998@gmail.com>
2019-12-06 13:46:45 +0200

    Find all tagNames for a question

--- src/main/java/src/service/TagQuestionService.java
+++ src/main/java/src/service/TagQuestionService.java
@@ -3,8 +3,11 @@ package src.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import src.domain.Question;
 import src.domain.TagQuestion;
 import src.repository.TagQuestionRepository;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
@@ -15,5 +18,12 @@ public class TagQuestionService {
 
     public TagQuestion save(TagQuestion tagQuestion) {
         return repository.saveAndFlush(tagQuestion);
+    }
+
+    public List<String> findAllTagsNameForQuestion(Question question) {
+        List<TagQuestion> tagQuestions = repository.findAllByQuestion(question);
+        return tagQuestions.stream()
+            .map(tagQuestion -> tagQuestion.getTag().getTagName())
+            .collect(Collectors.toList());
     }
 }
