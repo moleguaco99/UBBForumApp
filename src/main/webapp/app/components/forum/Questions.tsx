3ab81e6b7ffaa729854d62edd516773f9158a497
Dragos Serban <49645354+moleguaco99@users.noreply.github.com>
2019-12-11 23:25:41 +0200

    Introduced pressed-enter event
    Now press enter to add questions and replies

--- src/main/webapp/app/components/forum/Questions.tsx
+++ src/main/webapp/app/components/forum/Questions.tsx
@@ -3,8 +3,8 @@ import Card from '@material-ui/core/Card';
 import { CardContent } from '@material-ui/core';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-import { Redirect, Link } from 'react-router-dom'
-import { threadId } from 'worker_threads';
+import { Redirect, Link } from 'react-router-dom';
+import axios from 'axios';
 
 export class Questions extends Component<any, any>{
     intervals = [];
@@ -14,11 +14,18 @@ export class Questions extends Component<any, any>{
             questions:[],
             questionText: "",
             redirect: false,
-            highlightedQuestion: ""
+            highlightedQuestion: "",
+            userID: 0
         }
     }
     
     componentDidMount(){
+        axios.get('api/account').
+            /* eslint-disable no-console */
+            then(response => this.setState({
+                userID: response.data.id
+            }))
+
         fetch('http://localhost:8080/ourApi/questions/')
             .then(response=> response.json())
             .then(json => {
@@ -26,8 +33,7 @@ export class Questions extends Component<any, any>{
                     questions: json
                 })
             })
-        /* eslint-disable no-console */
-        .catch((error) => console.log(error))
+            .catch((error) => console.log(error))
 
         this.intervals.forEach(clearInterval);
         this.intervals.push(setInterval(this.getQuestions, 2000));  
@@ -84,7 +90,7 @@ export class Questions extends Component<any, any>{
         }
     }
 
-    askQuestion = (event) =>{
+    askQuestion = () =>{
         const newTags = []
         this.props.semester !== "" ? newTags.push(this.props.semester) : null;
         this.props.specialization !== "" ? newTags.push(this.props.specialization) : null;
@@ -92,7 +98,7 @@ export class Questions extends Component<any, any>{
         this.props.tags !== "" ? new Set(this.props.tags.split(",")).forEach(item => {
                                                 if(item !== "")
                                                 newTags.push(item); }) : null
-
+        
         fetch('http://localhost:8080/ourApi/question', {
             method:'POST',
             headers:{
@@ -101,7 +107,7 @@ export class Questions extends Component<any, any>{
             },
             body: JSON.stringify({
                 text: this.state.questionText,
-                idUser: 4,
+                idUser: this.state.userID,
                 tags: newTags,
             })
         }).catch((error) => {console.log(error)})
@@ -116,20 +122,30 @@ export class Questions extends Component<any, any>{
         })
     }
 
+    pressEnter = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        if(event.keyCode === 13){
+            this.askQuestion();
+        }
+    }
+
     render(){
         return(
             <div>
-            { this.state.redirect ?  <Redirect to={{ pathname:'/topic', state:{ from : this.state.highlightedQuestion} }} /> : null}
+            { this.state.redirect ?  <Redirect push to={{ pathname:'/topic', state:{ from : this.state.highlightedQuestion} }} /> : null}
             <div style={{height:"50vh", width:"85%", marginTop:"2%", marginLeft:"3%", border:"1px solid #F8F8FF", boxShadow:"0px 0px 1px black", overflow: 'auto'}}>
                 {this.state.questions.map(question => (
                     <div key={question.text}>
                         <div style={{display:"flex"}}>
                             {question.userP.imageUrl !== null ? 
-                                <img src={question.userP.imageUrl} style={{height:"50px", marginTop:"1%", alignSelf:"center", marginLeft:"3%", borderRadius:"50%" }}></img> 
-                                :
-                                <img src="content\images\user-icon.png" style={{height:"50px", marginTop:"1%", alignSelf:"center", marginLeft:"3%", borderRadius:"50%", border:"1px solid #D8D8D8"  }}></img>
+                                <img src={question.userP.imageUrl} 
+                                    style={{height:"50px", marginTop:"1%", alignSelf:"center", marginLeft:"3%", borderRadius:"50%" }} /> :
+                                <img src="content\images\user-icon.png"
+                                    style={{height:"50px", marginTop:"1%", alignSelf:"center", marginLeft:"3%", borderRadius:"50%", border:"1px solid #D8D8D8" }} />
                             }
-                            <Card onClick={() => {this.setState({redirect: true, highlightedQuestion: question})}} 
+                            <Card onClick={() => {this.setState({
+                                            redirect: true,
+                                            highlightedQuestion: question})
+                                        }} 
                                 style={{height:"45px", width:"80%", marginLeft:"1%", marginTop:"1%",  textAlign:"justify", display:"flex", backgroundColor:"#59C3C3", boxShadow:"0px 2px 2px #C6DBF0", color:"white"}}>
                                 <CardContent>
                                     {question.text}
@@ -141,7 +157,7 @@ export class Questions extends Component<any, any>{
                 ))} 
             </div>
                 <div style={{position:'relative', marginTop:'1%', marginLeft:'3%', width:'85%', display:'flex'}}>
-                    <TextField style={{ width:'85%'}} label="Ask a question" variant="outlined" onChange={this.handleQuestionText} value={this.state.questionText} />
+                    <TextField style={{ width:'85%'}} label="Ask a question" variant="outlined" onChange={this.handleQuestionText} onKeyUp={this.pressEnter} value={this.state.questionText} />
                     <Button variant="contained" size="small" style={{scale:'0.5', marginLeft:'1%', height:'50px', backgroundColor:'#1F75FE', color:'white'}} onClick={this.askQuestion}> Post question </Button>
                 </div>       
             </div>
--- src/main/webapp/app/components/forum/TopicPage.tsx
+++ src/main/webapp/app/components/forum/TopicPage.tsx
@@ -1,5 +1,7 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Card from '@material-ui/core/Card';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 import { CardContent } from '@material-ui/core';
 
 export class TopicPage extends React.Component<any, any>{
@@ -7,7 +9,30 @@ export class TopicPage extends React.Component<any, any>{
     constructor(props){
         super(props);
         this.state = {
-            replies:['reply#1', 'reply#2', 'reply#3']
+            replies:[],
+            replyText: ""
+        }
+    }
+
+    handleReplyText = (event) => {
+        this.setState({
+            replyText: event.target.value
+        })
+    }
+
+    replyTopic = () => {
+        this.setState(prevState => {
+            prevState.replies.push(prevState.replyText)
+            return {
+                replies: prevState.replies,
+                replyText: "" 
+            }
+        })
+    }
+
+    pressEnter = (event : React.KeyboardEvent<HTMLInputElement>) => {
+        if(event.keyCode === 13){
+            this.replyTopic();
         }
     }
 
@@ -18,8 +43,8 @@ export class TopicPage extends React.Component<any, any>{
                     {this.props.location.state.from.userP.imageUrl !== null ? 
                             <img src={this.props.location.state.from.userP.imageUrl} style={{height:"50px", marginTop:"1%", alignSelf:"center", marginLeft:"3%", borderRadius:"50%" }}></img> 
                             :
-                            <img src="content\images\user-icon.png" style={{height:"50px", marginTop:"1%", alignSelf:"center", marginLeft:"3%", borderRadius:"50%", border:"1px solid #D8D8D8"  }}></img>
-                    }
+                            <img src="content\images\user-icon.png" style={{height:"50px", marginTop:"1%", alignSelf:"center", marginLeft:"3%", borderRadius:"50%", border:"1px solid #D8D8D8"  }}></img>}
+
                     <Card style={{height:"45px", width:"80%", marginLeft:"1%", marginTop:"1%",  textAlign:"justify", display:"flex", backgroundColor:"#59C3C3", boxShadow:"0px 2px 2px #C6DBF0", color:"white"}}>
                         <CardContent>
                             {this.props.location.state.from.text}
@@ -29,15 +54,19 @@ export class TopicPage extends React.Component<any, any>{
                 <div>
                 {this.state.replies.map(reply => (
                     <div key={reply} style={{display:'flex', marginLeft:"5%"}}>
-                            <img src="content\images\user-icon.png" style={{height:"50px", marginTop:"1%", alignSelf:"center", marginLeft:"3%", borderRadius:"50%", border:"1px solid #D8D8D8"  }}></img>
-                            <Card 
-                                style={{height:"45px", width:"80%", marginTop:"1%", marginLeft:'1%', textAlign:"justify", display:"flex", backgroundColor:"#0D5C63", boxShadow:"0px 2px 2px #C6DBF0", color:"white"}}>
-                                <CardContent>
-                                    {reply}
-                                </CardContent>
-                            </Card>
-                        </div>
-                        ))} 
+                        <img src="content\images\user-icon.png" style={{height:"50px", marginTop:"1%", alignSelf:"center", marginLeft:"3%", borderRadius:"50%", border:"1px solid #D8D8D8"  }}></img>
+                        <Card 
+                            style={{height:"45px", width:"80%", marginTop:"1%", marginLeft:'1%', textAlign:"justify", display:"flex", backgroundColor:"#0D5C63", boxShadow:"0px 2px 2px #C6DBF0", color:"white"}}>
+                            <CardContent>
+                                {reply}
+                            </CardContent>
+                        </Card>
+                    </div> ))} 
+                </div>
+                <div style={{position:'relative', marginTop:'1%', marginLeft:'5%', width:'85%', display:'flex'}}>
+                    <TextField style={{ width:'85%'}} label="Give an answer" variant="outlined" onChange={this.handleReplyText} onKeyUp={this.pressEnter} value={this.state.replyText} />
+                    <Button variant="contained" size="small" style={{scale:'0.5', marginLeft:'1%', height:'50px', backgroundColor:'#1F75FE', color:'white'}}
+                            onClick={this.replyTopic}> Answer topic </Button>
                 </div>
             </div>
         )
