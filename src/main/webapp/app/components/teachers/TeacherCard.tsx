f6cd8f989c2b32373b4600ea2e33b8415620a1ff
CiprianT <tei_cip@yahoo.com>
2019-12-13 13:24:14 +0200

    first good version of teacher page

--- src/main/webapp/app/components/teachers/TeacherCard.tsx
+++ src/main/webapp/app/components/teachers/TeacherCard.tsx
@@ -1,13 +1,11 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import "../teachers/cardStyle.css"
+import React from 'react';
+import "../teachers/cardStyle.css";
 
 export default class TeacherCard extends React.Component{
   constructor(props){
@@ -17,9 +15,10 @@ export default class TeacherCard extends React.Component{
 
   render(){
   return (
-    <Card className="card">
+    <Card className="card" style={{padding: "auto", width:"40%", display:"flex", flexDirection:"row", paddingBlockStart:"0px", paddingInlineStart:"0px", paddingBlockEnd:"0px"}}>
+        <div><img style={{width:"138px", borderRadius:"3%"}} src={"http://www.cs.ubbcluj.ro/wp-content/uploads/Czibula-Istvan.jpg"}/></div>
+        <div style={{display:"flex", flexDirection:"column", width:"100%"}}>
       <CardActionArea>
-        <img src={"http://www.cs.ubbcluj.ro/wp-content/uploads/Czibula-Istvan.jpg"} style={{marginLeft:"-400px"}}/>
         <CardContent>
           <Typography gutterBottom variant="h5" component="h2">
             Professor
@@ -30,10 +29,13 @@ export default class TeacherCard extends React.Component{
         </CardContent>
       </CardActionArea>
       <CardActions>
-        <Button size="small" color="primary" style={{marginLeft:"420px"}}>
+        <div style={{marginLeft:"auto"}}>
+        <Button size="small" color="primary">
           <a href={"http://www.cs.ubbcluj.ro/~istvanc/"} target={"_blank"}>Go to teacher site</a>
         </Button>
+        </div>
       </CardActions>
+      </div>
     </Card>
   )
 }
