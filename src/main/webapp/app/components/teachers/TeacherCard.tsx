0f497d6ce4c00e6c1724394870034282ac01ba1d
CiprianT <tei_cip@yahoo.com>
2020-01-09 00:02:16 +0200

    asd

--- src/main/webapp/app/components/teachers/TeacherCard.tsx
+++ src/main/webapp/app/components/teachers/TeacherCard.tsx
@@ -1,46 +1,57 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import React from 'react';
+// import '../teachers/cardStyle.css';
 
-const useStyles = makeStyles({
-  card: {
-    maxWidth: 600,
-    marginBottom: 20,
-    marginLeft:"auto",
-    marginRight:"auto"
-  },
-  image: {
-    marginLeft:"-390px",
-  },
-});
+export default class TeacherCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-export default function TeacherCard() {
-  const classes = useStyles();
-
-  return (
-    <Card className={classes.card}>
-      <CardActionArea>
-        <img src={"http://www.cs.ubbcluj.ro/wp-content/uploads/Czibula-Istvan.jpg"} style={{marginLeft:"-400px"}}/>
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            Professor
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            Something about the teacher...Idk
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary" style={{marginLeft:"420px"}}>
-          <a href={"http://www.cs.ubbcluj.ro/~istvanc/"} target={"_blank"}>Go to teacher site</a>
-        </Button>
-      </CardActions>
-    </Card>
-  );
+  render() {
+    return (
+      <Card
+        className="card"
+        style={{
+          padding: 'auto',
+          width: '40%',
+          display: 'flex',
+          flexDirection: 'row',
+          paddingBlockStart: '0px',
+          paddingInlineStart: '0px',
+          paddingBlockEnd: '0px'
+        }}
+      >
+        <div>
+          <img style={{ width: '138px', borderRadius: '3%' }} src={'http://www.cs.ubbcluj.ro/wp-content/uploads/Czibula-Istvan.jpg'} />
+        </div>
+        <div style={{ display: 'flex', flexDirection: 'column', width: '100%' }}>
+          <CardActionArea>
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                Professor
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                Details about the teacher.
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <div style={{ marginLeft: 'auto' }}>
+              <Button size="small" color="primary">
+                <a href={'http://www.cs.ubbcluj.ro/~istvanc/'} target={'_blank'}>
+                  Go to teacher site
+                </a>
+              </Button>
+            </div>
+          </CardActions>
+        </div>
+      </Card>
+    );
+  }
 }
