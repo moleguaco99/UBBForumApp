cdcd92233832a55a26697ac975b12f21241e1f6f
CiprianT <tei_cip@yahoo.com>
2020-01-10 12:01:33 +0200

    teachers_page

--- src/main/webapp/app/components/teachers/TeachersPage.tsx
+++ src/main/webapp/app/components/teachers/TeachersPage.tsx
@@ -1,15 +1,32 @@
 import React from 'react';
 import TeacherCard from '../teachers/TeacherCard';
+import SearchBar from 'material-ui-search-bar';
 
 export const TeachersPage = () => {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <h1>Teachers</h1>
-      <TeacherCard />
-      <TeacherCard />
-      <TeacherCard />
-      <TeacherCard />
-      <TeacherCard />
+    <div style={{ textAlign: 'center', display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
+      <div style={{ flexGrow: 0.2 }}>
+        <h1>Filters</h1>
+        <SearchBar
+          onChange={() => {}}
+          onRequestSearch={() => {}}
+          style={{
+            margin: '0 auto',
+            maxWidth: 800
+          }}
+        />
+      </div>
+      <div style={{ flexGrow: 2 }}>
+        <h1>Teachers</h1>
+        <TeacherCard />
+        <TeacherCard />
+        <TeacherCard />
+        <TeacherCard />
+        <TeacherCard />
+      </div>
+      <div style={{ flexGrow: 0.5 }}>
+        <h1>Details</h1>
+      </div>
     </div>
   );
 };
